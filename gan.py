52bd4dc618419404676d8717df97c66f25062009
Oleg Klimov <omgtech@gmail.com>
2016-12-07 13:50:35 +0300

    Labels on image

--- gan.py
+++ gan.py
@@ -23,17 +23,21 @@ from keras.datasets import cifar10
 #print X_train.shape  # (50000, 3, 32, 32)
 #print y_train        # (50000, 1)            [[0], [1], [9]]  
 
-X_train = X_train.astype('float32')
-X_test  = X_test.astype('float32')
-X_train /= 255
-X_test  /= 255
-
-y_train = np_utils.to_categorical(y_train, 10).astype('float32')
-y_test  = np_utils.to_categorical(y_test,  10).astype('float32')
-
-BATCH = 100
+BATCH  = 100
 LATENT = 150
 H,W = X_train.shape[-2:]
+LABELS = 10
+labels_text = "airplane automobile bird cat deer dog frog horse ship truck FAKE".split()
+
+X_train = X_train.astype('float32')
+X_test  = X_test.astype('float32')
+X_train /= 128
+X_train -= 1.0
+X_test  /= 128
+X_test  -= 1.0
+
+y_train = np_utils.to_categorical(y_train, LABELS+1).astype('float32')
+y_test  = np_utils.to_categorical(y_test,  LABELS+1).astype('float32')
 
 class Gan:
 	def __init__(self):
@@ -51,7 +55,7 @@ class Gan:
 		b6    = BatchNormalization(mode=0, axis=1)
 		flat = Flatten()
 		d1 = Dense(256, activation='relu')
-		d2 = Dense(10, activation='softmax')
+		d2 = Dense(LABELS+1, activation='softmax')
 
 		inp = Input( batch_shape=(None,3,H,W), name="inp_train" )
 		z = b1(conv1(inp))
@@ -63,31 +67,9 @@ class Gan:
 		z = d1(flat(z))
 		class1 = d2(z)
 
-		lat_dense1 = Dense(256, activation='relu')
-		lat_dense1_bn = BatchNormalization(mode=0, axis=1)
-		lat_dense2 = Dense(256*W/8*H/8, activation='relu')
-		lat_dense2_bn = BatchNormalization(mode=0, axis=1)
-		lat_reshape = Reshape( (256,H/8,W/8) )
-		dec1 = Deconvolution2D(128, 3, 3, output_shape=(None,128,H/4,W/4), subsample=(2,2), border_mode='same')
-		dec1_bn = BatchNormalization(mode=0, axis=1)
-		dec2 = Deconvolution2D(64, 3, 3, output_shape=(None,64,H/2,W/2), subsample=(2,2), border_mode='same')
-		dec2_bn = BatchNormalization(mode=0, axis=1)
-		dec3 = Deconvolution2D(32, 3, 3, output_shape=(None,32,H,W), subsample=(2,2), border_mode='same')
-		dec3_bn = BatchNormalization(mode=0, axis=1)
-		dec4 = Deconvolution2D(3, 3, 3, output_shape=(None,3,H,W), border_mode='same')
-
-		latent = Input( batch_shape=(None,LATENT), name="latent" )
-		g = lat_dense1_bn(lat_dense1(latent))
-		g = lat_dense2_bn(lat_dense2(g))
-		g = lat_reshape(g)
-		g = dec1_bn(dec1(g))
-		g = dec2_bn(dec2(g))
-		g = dec3_bn(dec3(g))
-		g = dec4(g)
-
 		self.class_model = Model( input=inp, output=class1 )
 		def modified_categorical_crossentropy(y_true, y_pred):
-			y_pred *= 0.9
+			y_pred *= 0.9/y_pred.sum(axis=-1, keepdims=True)
 			_EPSILON = K.epsilon()
 			return K.mean(K.sum(-y_true*K.log(K.clip(y_pred, _EPSILON, 1-_EPSILON)), axis=-1))
 		self.class_model.compile(
@@ -95,9 +77,36 @@ class Gan:
 			loss=modified_categorical_crossentropy,
 			metrics=['accuracy'])
 
-		fake_merged_with_real = merge( [inp,g], mode='concat', concat_axis=0 )
+		lat_dense1 = Dense(256, activation='relu')
+		lat_dense2 = Dense(256*W/8*H/8, activation='relu')
+		lat_reshape = Reshape( (256,H/8,W/8) )
+		dec1 = Deconvolution2D(128, 3, 3, activation='relu', output_shape=(None,128,H/4,W/4), subsample=(2,2), border_mode='same')
+		dec1_bn = BatchNormalization(mode=0, axis=1)
+		dec2 = Deconvolution2D(64, 3, 3, activation='relu', output_shape=(None,64,H/2,W/2), subsample=(2,2), border_mode='same')
+		dec2_bn = BatchNormalization(mode=0, axis=1)
+		dec3 = Deconvolution2D(32, 3, 3, activation='relu', output_shape=(None,32,H,W), subsample=(2,2), border_mode='same')
+		dec3_bn = BatchNormalization(mode=0, axis=1)
+		dec4 = Deconvolution2D(3, 3, 3, activation='tanh', output_shape=(None,3,H,W), border_mode='same')
 
-		self.discriminator_model = Model( input=fake_merged_with_real, output=class1 )
+		latent = Input( batch_shape=(None,LATENT), name="latent" )
+		g = lat_dense1(latent)
+		g = lat_dense2(g)
+		g = lat_reshape(g)
+		g = dec1_bn(dec1(g))
+		g = dec2_bn(dec2(g))
+		g = dec3_bn(dec3(g))
+		g = dec4(g)
+		fake_merged_with_real = merge( [inp,g], mode='concat', concat_axis=0 )
+		z = b1(conv1(fake_merged_with_real))
+		z = b2(conv2(z))
+		z = b3(conv3(z))
+		z = b4(conv4(z))
+		z = b5(conv5(z))
+		z = b6(conv6(z))
+		z = d1(flat(z))
+		class_discr = d2(z)
+
+		self.discriminator_model = Model( input=[inp,latent], output=class_discr )
 		self.discriminator_model.compile(
 			optimizer=Adam(lr=0.002, beta_2=0.999),
 			loss=modified_categorical_crossentropy,
@@ -105,32 +114,60 @@ class Gan:
 
 		for x in [conv1,b1,conv2,b2,conv3,b3,conv4,b4,conv5,b5,conv6,b6,d1,d2]:
 			x.trainable = False   # should do it before compiling generator
+		z = b1(conv1(g))
+		z = b2(conv2(z))
+		z = b3(conv3(z))
+		z = b4(conv4(z))
+		z = b5(conv5(z))
+		z = b6(conv6(z))
+		z = d1(flat(z))
+		class_generator = d2(z)
 
-		self.generator_fool_model = Model( input=latent, output=g )
+		def modified_categorical_crossentropy2(y_true, y_pred):
+			y_pred *= 0.9/y_pred.sum(axis=-1, keepdims=True)
+			_EPSILON = K.epsilon()
+			return K.mean(K.sum(-y_true*K.log(K.clip(y_pred, _EPSILON, 1-_EPSILON)), axis=-1))
+		self.generator_fool_model = Model( input=latent, output=class_generator )
 		self.generator_fool_model.compile(
-			optimizer=Adam(lr=0.002, beta_2=0.999),
-			loss=modified_categorical_crossentropy,
+			optimizer=Adam(lr=0.001, beta_2=0.999),
+			loss=modified_categorical_crossentropy2,
 			metrics=['accuracy'])
 
 gan = Gan()
-gan.class_model.summary()
+#gan.class_model.summary()
+gan.generator_fool_model.summary()
 
 import scipy
 import scipy.misc
 import scipy.misc.pilutil
 
-def batch_to_jpeg(batch, fn="test.png"):
+def batch_to_jpeg(batch, labels, fn="test.png"):
 	BATCH = batch.shape[0]
 	W = batch.shape[-1]
 	H = batch.shape[-2]
 	side = int( np.ceil(np.sqrt(BATCH)) )
-	pic = np.zeros( shape=(3,side*H,side*W) )
+	BIG_W = W + 32
+	BIG_H = H + 20
+	pic = np.ones( shape=(3,side*BIG_H,side*BIG_W) )
+
+	import PIL
+	import PIL.Image as Image
+	import PIL.ImageDraw as ImageDraw
+	import PIL.ImageFont as ImageFont
+	font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeSansBold.ttf', 10)
 	for y in range(side):
 		for x in range(side):
 			i = y*side + x
 			if i > BATCH: break
-			pic[:, y*H:(y+1)*H, x*W:(x+1)*W] = batch[i]
-	scipy.misc.toimage(pic, cmin=0.0, cmax=1.0).save(fn)
+			pic[:, y*BIG_H:y*BIG_H+H, x*BIG_W:x*BIG_W+W] = batch[i]
+	image = scipy.misc.toimage(pic, cmin=-1.0, cmax=1.0)
+	draw = ImageDraw.Draw(image)
+	for y in range(side):
+		for x in range(side):
+			i = y*side + x
+			if i > BATCH: break
+			draw.text((x*BIG_W, y*BIG_H+H), labels[i], font=font, fill='rgb(0, 0, 0)')
+	image.save(fn)
 
 cursor = 1e10
 epoch = 0
@@ -141,7 +178,7 @@ while 1:
 		shuffled_y_train = y_train[sh]
 		cursor = 0
 		epoch += 1
-		loss, acc = gan.class_model.evaluate( X_test, y_test, batch_size=BATCH)
+		loss, acc = gan.class_model.evaluate( X_test, y_test, batch_size=BATCH )
 		print "e%i test ---- loss=%0.3f acc=%0.2f%%" % (epoch, loss, acc*100)
 
 	x = shuffled_x_train[cursor:cursor+BATCH]
@@ -150,7 +187,11 @@ while 1:
 	loss, acc = gan.class_model.train_on_batch( x, y )
 	if cursor % 10000 == 0:
 		print "e%i:%05i loss=%0.3f acc=%0.2f%%" % (epoch, cursor, loss, acc*100)
-		batch_to_jpeg( shuffled_x_train[:BATCH] )
+		labels = gan.class_model.predict( x )
+		batch_labels_text = []
+		for b in range(BATCH):
+			batch_labels_text.append( labels_text[labels[b].argmax()] )
+		batch_to_jpeg( x, batch_labels_text )
 
 	cursor += BATCH
 
