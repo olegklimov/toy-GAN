fe90c090ffbebb8b825f52e0f34868bff25ee184
Oleg Klimov <omgtech@gmail.com>
2017-01-14 19:32:03 +0300

    Kind of works

--- GAN-tf.py
+++ GAN-tf.py
@@ -44,13 +44,13 @@ def deconv_wn(x, name, kernel_shape, num_filters, stride, wn_init):
 
 def discriminator_network(x, wn_init):
     h, w, features = x.get_shape().as_list()[-3:]
-    print("classifier input h w f = %i %i %i" % (h,w,features))
+    print("discriminator input h w f = %i %i %i" % (h,w,features))
     x = leaky_relu( W.conv2d_wn(x,  32, "conv11", [3,3], [1,1], wn_init=wn_init) )
     x = leaky_relu( W.conv2d_wn(x,  64, "conv21", [3,3], [2,2], wn_init=wn_init) )
     x = leaky_relu( W.conv2d_wn(x, 128, "conv31", [3,3], [2,2], wn_init=wn_init) )
     x = leaky_relu( W.conv2d_wn(x, 256, "conv41", [3,3], [2,2], wn_init=wn_init) )
-    h, w, features = x.get_shape().as_list()[-3:]
-    print("discriminator final h w f = %i %i %i -> flat %i" % (h,w,features, h*w*features))
+    batch, h, w, features = x.get_shape().as_list()
+    print("discriminator final b h w f = %s %i %i %i -> flat %i" % (batch,h,w,features, h*w*features))
     x = tf.reshape(x, [-1,h*w*features])
     x = leaky_relu( W.dense_wn(x, 256, "dense1", wn_init=wn_init) )
     return x
@@ -59,14 +59,14 @@ def generator_network(l, wn_init):
     batch_size,features = l.get_shape().as_list()
     print("generator input b f = %s %i" % (batch_size,features))
     h,w,features = 4,4,512
-    x = leaky_relu( W.dense_wn(l, h*w*features, "latent_fc", wn_init=wn_init) )
+    x = tf.nn.relu( W.dense_wn(l, h*w*features, "latent_fc", wn_init=wn_init) )
     x = tf.reshape(x, [-1,h,w,features])
     want_features = 512
-    x = leaky_relu( deconv_wn(x, "deconv1", [3,3,want_features,features], features, 2, wn_init=wn_init) )
+    x = tf.nn.relu( deconv_wn(x, "deconv1", [3,3,want_features,features], features, 2, wn_init=wn_init) )
     features, want_features = want_features, 256
-    x = leaky_relu( deconv_wn(x, "deconv2", [3,3,want_features,features], features, 2, wn_init=wn_init) )
+    x = tf.nn.relu( deconv_wn(x, "deconv2", [3,3,want_features,features], features, 2, wn_init=wn_init) )
     features, want_features = want_features, 128
-    x = leaky_relu( deconv_wn(x, "deconv3", [3,3,want_features,features], features, 2, wn_init=wn_init) )
+    x = tf.nn.relu( deconv_wn(x, "deconv3", [3,3,want_features,features], features, 2, wn_init=wn_init) )
     x = tf.nn.tanh( W.conv2d_wn(x, dataset.COLORS, "tocolors",  [1,1], [1,1], wn_init=wn_init) )
     h, w, features = x.get_shape().as_list()[-3:]
     print("generator final h w f = %i %i %i" % (h,w,features))
@@ -74,7 +74,7 @@ def generator_network(l, wn_init):
 
 def do_all():
     config = tf.ConfigProto()
-    #config.gpu_options.per_process_gpu_memory_fraction = 0.8
+    config.gpu_options.per_process_gpu_memory_fraction = 0.6
     #config.gpu_options.allow_growth = True
     #config=tf.ConfigProto(log_device_placement=True))
     sess = tf.InteractiveSession(config=config)
@@ -82,52 +82,66 @@ def do_all():
     real = tf.placeholder(tf.float32, [None,dataset.H,dataset.W,1], name='real_input')
 
     classification_true = tf.placeholder(tf.float32, [None,dataset.LABELS], name='classification_true')
+    klass_true = tf.placeholder(tf.float32, [None,dataset.LABELS], name='klass_true')
     real_or_fake_true   = tf.placeholder(tf.float32, [None,2], name='real_or_fake_true')
     latent_placeholder  = tf.placeholder(tf.float32, [dataset.BATCH,LATENT], name='latent')
 
     gen_wm_init = W.WeightNormInitializer()
-    with tf.name_scope("generator_network"):
+    with tf.variable_scope("generator_network"):
         fake = generator_network(latent_placeholder, gen_wm_init)
         generator_learnable = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
-    gen_wm_init.dump_to_tf_summary()
+        print("generator_learnable: \n %s" % "\n ".join(v.name for v in generator_learnable))
 
     real_concat_fake = U.concatenate([real,fake], axis=0)
 
     disc_wm_init = W.WeightNormInitializer()
-    with tf.name_scope("discriminator_network"):
-        last = discriminator_network(real_concat_fake, disc_wm_init)
+    class_wm_init = W.WeightNormInitializer()
+    with tf.variable_scope("discriminator_network"):
+        last1 = discriminator_network(real, class_wm_init)
+        x = U.dense(last1, dataset.LABELS, "dense10", weight_init=U.normc_initializer(1.0))
+        klass_loss     = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(x, klass_true))
+        klass_accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(x, 1), tf.argmax(klass_true, 1)), tf.float32))
 
-        x = U.dense(last, dataset.LABELS, "dense10")
+        tf.get_variable_scope().reuse_variables()
+        last2  = discriminator_network(real_concat_fake, disc_wm_init)
+        x = U.dense(last2, dataset.LABELS, "dense10")
         classification_logits   = tf.nn.softmax(x)
         classification_loss     = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(x, classification_true))
         classification_accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(x, 1), tf.argmax(classification_true, 1)), tf.float32))
-        tf.summary.scalar('classification_loss', classification_loss)
-        tf.summary.scalar('classification_accuracy', classification_accuracy)
 
-        x = U.dense(last, 2, "dense_real_or_fake")
+        discriminator_trainable = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
+        print("disc trainable: \n %s" % "\n ".join(v.name for v in discriminator_trainable))
+
+    with tf.variable_scope("disc_losses"):
+        x = U.dense(last2, 2, "dense_real_or_fake")
         real_or_fake_logits   = tf.nn.softmax(x)
         real_or_fake_loss     = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(x, real_or_fake_true))
         real_or_fake_accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(x, 1), tf.argmax(real_or_fake_true, 1)), tf.float32))
-        tf.summary.scalar('real_or_fake_loss', real_or_fake_loss)
-        tf.summary.scalar('real_or_fake_accuracy', real_or_fake_accuracy)
 
-        discriminator_learnable = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
-    disc_wm_init.dump_to_tf_summary()
+    tf.summary.scalar('classification_loss', classification_loss)
+    tf.summary.scalar('classification_accuracy', classification_accuracy)
+    tf.summary.scalar('klass_loss', klass_loss)
+    tf.summary.scalar('klass_accuracy', klass_accuracy)
+    tf.summary.scalar('real_or_fake_loss', real_or_fake_loss)
+    tf.summary.scalar('real_or_fake_accuracy', real_or_fake_accuracy)
 
     # summary
+    gen_wm_init.dump_to_tf_summary()
+    disc_wm_init.dump_to_tf_summary()
     merged = tf.summary.merge_all()
     train_writer = tf.summary.FileWriter(LOG_TRAIN_DIR, sess.graph)
     test_writer = tf.summary.FileWriter(LOG_TEST_DIR)
 
     # adams
     with tf.name_scope('adam_discriminator'):
-        adam_discriminator = tf.train.AdamOptimizer(0.0002, beta1=0.5).minimize(
-            real_or_fake_loss, # + classification_loss,
-            var_list=discriminator_learnable)
+        adam_discriminator = tf.train.AdamOptimizer(0.00001, beta1=0.5).minimize(
+            klass_loss + real_or_fake_loss,
+            var_list=discriminator_trainable)
     with tf.name_scope('adam_generator'):
-        adam_generator = tf.train.AdamOptimizer(0.0002, beta1=0.5).minimize(
-            (-1.0)*real_or_fake_loss, # + classification_loss
+        adam_generator = tf.train.AdamOptimizer(0.00001, beta1=0.5).minimize(
+            (-1.0)*real_or_fake_loss + classification_loss,
             var_list=generator_learnable)
+    assert( not (set(generator_learnable) & set(discriminator_trainable)) )
 
     tf.global_variables_initializer().run()
     # stop_gradient(2*x) - x
@@ -142,16 +156,17 @@ def do_all():
             ts2 = time.time()
             if ts1:
                 print('e%02i:%05i %0.2fms acc=%0.2f' % (dataset.epoch, step, 1000*(ts2-ts1), acc*100))
-            run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-            run_metadata = tf.RunMetadata()
+            #run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            run_options = None
+            run_metadata = None   #tf.RunMetadata()
         else:
             run_options = None
             run_metadata = None
 
         d_real, d_labels = dataset.next_batch()
         d_real  = d_real.copy()
-        d_real[:,:,:,:] += np.random.uniform(low=-1, high=+1, size=(dataset.BATCH,dataset.H,dataset.W,1))
-        label_leak = 0.2
+        d_real[:,:,:,:] += np.random.normal(0, 0.5, size=(dataset.BATCH,dataset.H,dataset.W,1))
+        label_leak = 0.0
         d_latent         = np.random.normal(0, 1, size=(dataset.BATCH,LATENT) )
         d_class          = np.zeros( (2*dataset.BATCH,dataset.LABELS), dtype=np.float32 )
         d_class[:dataset.BATCH] = d_labels*(1-label_leak-label_leak/dataset.LABELS)
@@ -161,7 +176,7 @@ def do_all():
         real_or_fake[:dataset.BATCH,0] = 0.9
         real_or_fake[:dataset.BATCH,1] = 0
         real_or_fake[dataset.BATCH:,0] = 0
-        real_or_fake[dataset.BATCH:,1] = 0.9
+        real_or_fake[dataset.BATCH:,1] = 1.0
 
         for b in range(dataset.BATCH):
             i = d_latent[b,:dataset.LABELS].argmax()
@@ -172,24 +187,36 @@ def do_all():
         feed_dict = {
             real: d_real,
             latent_placeholder: d_latent,
+            klass_true: d_class[:dataset.BATCH],
             classification_true: d_class,
             real_or_fake_true: real_or_fake,
             }
 
         if first_batch:
             first_batch = False
-            gen_wm_init.data_based_initialization(feed_dict)
-            disc_wm_init.data_based_initialization(feed_dict)
+            #gen_wm_init.data_based_initialization(feed_dict)
+            #disc_wm_init.data_based_initialization({ real: dataset.X_test })
+            class_wm_init.data_based_initialization({ real: dataset.X_test })
 
-        summary, _, _ = sess.run(
-            [merged, adam_discriminator, adam_generator],
-            options=run_options,
-            feed_dict=feed_dict,
-            run_metadata=run_metadata)
+        _ = sess.run(
+            [adam_generator],
+            feed_dict=feed_dict)
 
-        if run_metadata:
-            train_writer.add_run_metadata(run_metadata, 'step%05i' % step)
-        train_writer.add_summary(summary, step)
+        if step % 10 == 0:
+            summary, _ = sess.run(
+                [merged, adam_discriminator],
+                options=run_options,
+                feed_dict=feed_dict,
+                run_metadata=run_metadata)
+            train_writer.add_summary(summary, step)
+            #if run_metadata:
+            #    train_writer.add_run_metadata(run_metadata, 'step%05i' % step)
+        else:
+            _ = sess.run(
+                [adam_discriminator],
+                options=run_options,
+                feed_dict=feed_dict)
+
         ts1 = ts2
         sys.stdout.write(".")
         sys.stdout.flush()
@@ -198,7 +225,7 @@ def do_all():
             sys.stdout.write("j")
             sys.stdout.flush()
             real_concat_fake_export, classification_export = sess.run(
-                [real_concat_fake, real_or_fake_logits], # classification_logits
+                [real_concat_fake, classification_logits],
                 feed_dict = {
                     real: d_real,
                     latent_placeholder: d_latent,
